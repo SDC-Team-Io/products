fe1d09b6060848e223e36a6b652f66a2d82ed2a8
PJ O'Leary <peej@oleary.com>
2023-12-07 17:55:26 -0800

    Implemented functioning server routes

--- router.js
+++ router.js
@@ -5,9 +5,38 @@ const productsSchema = require('./db/productsSchema')
 
 const products = mongoose.model('products', productsSchema);
 
+
+router.get('/products', (req, res) => {
+
+  const page = req.query.page || 1;
+  const count = req.query.count || 5;
+
+  products.find({}, {
+    _id: 0,
+    product_id: 1,
+    name: 1,
+    slogan: 1,
+    description: 1,
+    category: 1,
+    default_price: 1,
+  })
+    .sort({ product_id: 1 })
+    .skip((count * page) - count)
+    .limit(count)
+  .then((data) => {
+    if (!data) res.send(404);
+    res.send(data);
+  })
+  .catch(() => {
+    res.status(500).send('Error fetching products');
+  })
+})
+
+
 router.get('/products/:id', (req, res) => {
-  console.log('Serving GET at endpoint /products for ip ' + req.ip);
+
   products.findOne({product_id: req.params.id}, {
+    _id: 0,
     product_id: 1,
     name: 1,
     slogan: 1,
@@ -17,9 +46,44 @@ router.get('/products/:id', (req, res) => {
     features: 1,
   })
   .then((data) => {
+    if (!data) res.send(404);
     data.id = data.product_id;
     res.send(data);
   })
+  .catch(() => {
+     res.status(500).send('Error fetching product ' + req.params.id);
+  })
 })
+
+
+router.get('/products/:id/styles', (req, res) => {
+  products.findOne({product_id: req.params.id}, {
+    _id: 0,
+    product_id: 1,
+    styles: 1,
+  })
+  .then((data) => {
+    if (!data) res.send(404);
+    res.send(data);
+  })
+  .catch(() => {
+    res.status(500).send('Error fetching styles for product ' + req.params.id);
+  })
+})
+
+router.get('/products/:id/related', (req, res) => {
+  products.findOne({product_id: req.params.id}, {
+    _id: 0,
+    related: 1,
+  })
+  .then((data) => {
+    if (!data) res.send(404);
+    res.send(data.related);
+  })
+  .catch(() => {
+    res.status(500).send('Error fetching related for product ' + req,params.id);
+  })
+})
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const router = require('./router');
 
 const app = express();
@@ -10,14 +11,20 @@ const url = 'mongodb://localhost:27017/products'
 const certificate = path.join(__dirname, '/db/mongoose-certificate.pem');
 
 
-
+app.use(morgan('tiny'));
 app.use('/', router);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './public/dist')));
 
+console.log('Connecting to MongoDB...');
+
 mongoose.connect(url)
 .then(() => {
+  console.log('Connected!');
   app.listen(port, () => {
     console.log('Products Server Live on Port ' + port);
   })
+})
+.catch(() => {
+  console.error('Error connecting to Mongo Database')
 })
